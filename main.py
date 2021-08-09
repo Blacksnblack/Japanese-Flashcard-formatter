c5d4f206886962184065b4f0648490ec85ca7bf1
Terry Black <blacksnblack@gmail.com>
2021-08-08 21:20:49 -0600

    Update main.py
    removed debugging stuff

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ with open(filename, "r", encoding="utf8") as f:
             # print(line)
             formattedFile = open(f"{line[1:-1]}.txt", "w+", encoding="utf8")
         elif len(line) > 1:
-            print(len(line))
-
             fData = []
             temp = line.split("–")
             [fData.append(data) for data in temp[0].split(" ") if len(data) > 0]
             if temp and len(temp) > 0:
                 fData.append(temp[1][:-1])
-
             if formattedFile is not None:
                 formattedFile.write(f"{fData[0]}-{fData[1]}\n{fData[2]}\n\n")
     formattedFile.close()
